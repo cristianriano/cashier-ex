24bd002231e4b890663e0847515c3bc8a628ad35
Cristian Riano <cristian.riano@glovoapp.com>
2021-11-28 16:19:12 +0100

    Create Product struct

--- test/cashier_test.exs
+++ test/cashier_test.exs
@@ -1,8 +1,24 @@
 defmodule CashierTest do
-  use ExUnit.Case
+  @moduledoc """
+    Public interface tests
+  """
+
+  use ExUnit.Case, async: true
   doctest Cashier
 
-  test "greets the world" do
-    assert Cashier.hello() == :world
+  describe ".get_product_info" do
+    test "returns the product info" do
+      {:ok, product} = Cashier.get_product_info("GR1")
+
+      assert product.code == "GR1"
+      assert product.name == "Green Tea"
+      assert product.price == 3.11
+    end
+
+    test "returns :not_found when products do not exists" do
+      {:error, reason} = Cashier.get_product_info("invalid")
+
+      assert reason == :not_found
+    end
   end
 end
