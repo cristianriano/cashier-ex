84934d0e635fc1b10c89f436fc4f34831ea71fed
Cristian Riano <cristian.riano@glovoapp.com>
2021-11-29 19:46:31 +0100

    Add ReducedPriceRule implementation

--- lib/cashier/rules/reduced_price_rule.ex
+++ lib/cashier/rules/reduced_price_rule.ex
@@ -11,7 +11,16 @@ defmodule Cashier.Rules.ReducedPriceRule do
   def init(%{target: target, min: min, new_price: new_price} = args) when is_binary(target) do
     %Rule{
       target: target,
-      process: fn basket -> basket end
+      process: fn %Basket{products_by_code: products, quantities_by_code: quantities} = basket ->
+        case quantities[target] do
+          q when q >= min ->
+            discount = (products[target].price - new_price) * q
+            Basket.reduce_total_price(basket, discount)
+
+          _ ->
+            basket
+        end
+      end
     }
   end
 end
