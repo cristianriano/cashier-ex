9f6577385362410640932811ec79592d0a9780d6
Cristian Riano <cristian.riano@glovoapp.com>
2021-11-29 19:50:28 +0100

    Add FractionPriceRule implementation

--- lib/cashier/rules/fraction_price_rule.ex
+++ lib/cashier/rules/fraction_price_rule.ex
@@ -3,6 +3,7 @@ defmodule Cashier.Rules.FractionPriceRule do
     This rule represents getting a fraction of the original price when buying in bulk
   """
 
+  alias Cashier.Basket
   alias Cashier.Rule
 
   @behaviour Rule
@@ -10,7 +11,16 @@ defmodule Cashier.Rules.FractionPriceRule do
   def init(%{target: target, min: min, fraction: fraction} = args) when is_binary(target) do
     %Rule{
       target: target,
-      process: fn basket -> basket end
+      process: fn %Basket{products_by_code: products, quantities_by_code: quantities} = basket ->
+        case quantities[target] do
+          q when (q >= min) and (fraction < 1.0) ->
+            discount = (products[target].price * (1 - fraction)) * q
+            Basket.reduce_total_price(basket, discount)
+
+          _ ->
+            basket
+        end
+      end
     }
   end
 end
